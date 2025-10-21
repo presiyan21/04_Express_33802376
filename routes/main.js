a22bc5a5426da3079913231fa4a4a2bed23909c0
presiyan21 <149686177+presiyan21@users.noreply.github.com>
2025-10-21 16:40:05 +0100

    04_express_33802376

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,7 @@
 // Create a new router
 const express = require("express");
 const router = express.Router();
+const path = require('path');
 
 // Home route
 router.get("/", (req, res) => res.send("Hello World!"));
